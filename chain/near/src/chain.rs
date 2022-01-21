c9038288d52cf1bbcdb84b6d388cf195cd970b3d
Matthieu Vachon <matt@streamingfast.io>
2022-01-21 14:09:23 -0500

    chain,graph,store: added code to properly resume from a subgraph block ptr and no firehose cursor
    Firehose is now using the subgraph ptr if no cursor is found. To ensure we do not create inconsistency in the data due to how Firehose resolves current block number, a sanity check has now been added. This check ensure that the first received block's parent from firehose is the same as the subgraph pointer block. If it's not the case, we revert to the last know final block so we are sure to start from a correct chain segment.
    
    What final block to pick is decided by the chain itself. On Ethereum, we use `block.number - 200` (underflow guarded) while on NEAR, we use the last final block height defined by the block itself.

--- chain/near/src/chain.rs
+++ chain/near/src/chain.rs
@@ -169,7 +169,7 @@ impl Blockchain for Chain {
         };
 
         firehose_endpoint
-            .irreversible_block_ptr_for_number::<codec::HeaderOnlyBlock>(logger, number)
+            .block_ptr_for_number::<codec::HeaderOnlyBlock>(logger, number)
             .map_err(Into::into)
             .await
     }
@@ -341,7 +341,7 @@ impl FirehoseMapperTrait<Chain> for FirehoseMapper {
         let final_block_number = block.header().last_final_block_height as BlockNumber;
 
         self.endpoint
-            .irreversible_block_ptr_for_number::<codec::HeaderOnlyBlock>(logger, final_block_number)
+            .block_ptr_for_number::<codec::HeaderOnlyBlock>(logger, final_block_number)
             .await
     }
 }
